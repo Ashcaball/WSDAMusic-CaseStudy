81ebc127c779247c8f2bc36248ae0e9a93f47014
Ashley Caballero <106620855+Ashcaball@users.noreply.github.com>
2025-04-02 15:20:18 -0400

    Update SQLCode.sql

--- SQLCode.sql
+++ SQLCode.sql
@@ -182,3 +182,15 @@ WHERE
 	InvoiceDate >
 (SELECT 
 	InvoiceDate FROM Invoice WHERE InvoiceId = 251)
+
+/* Subquery | Returning Multiple Values */
+
+SELECT 
+	InvoiceDate,
+	BillingAddress,
+	BillingCity
+FROM
+	Invoice
+WHERE
+	InvoiceDate IN
+(SELECT InvoiceDate FROM Invoice WHERE InvoiceId IN (251, 252, 254))
