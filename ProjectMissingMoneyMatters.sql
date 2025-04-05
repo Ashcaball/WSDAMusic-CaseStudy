801714a7afb3e2c9c08763f8c7e0e05ab7fcaa95
Ashley Caballero <106620855+Ashcaball@users.noreply.github.com>
2025-04-05 17:09:06 -0400

    Update ProjectMissingMoneyMatters.sql

--- ProjectMissingMoneyMatters.sql
+++ ProjectMissingMoneyMatters.sql
@@ -1,5 +1,5 @@
-/* The situation: For a long time, Adams Andrew, Manager of WSDA Music, has been unable to account for a discrepancy in his company’s financials. The furthest he has gotten in his own attempts to analyze the company data is figuring out that 
-the discrepancy occurred between the years 2011 and 2012. But that’s about all that Adams knows for certain. 
+/* The situation: For a long time, Adams Andrew, Manager of WSDA Music, has been unable to account for a discrepancy in his company’s financials. The furthest he has gotten in his own 
+attempts to analyze the company data is figuring out that the discrepancy occurred between the years 2011 and 2012. But that’s about all that Adams knows for certain. 
 
 You have been called in to do what you do best—apply your SQL skills:
  • Analyze WSDA Music’s Data to:
